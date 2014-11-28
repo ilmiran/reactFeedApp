e7ee602cfdfb0bb4185f31b8f71b8e97f8300dce
mirarichy <inugumanova@inugumanov-ltm2.internal.salesforce.com>
2014-11-28 10:39:04 -0800

    fix delete

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.all('/*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header( 'Access-Control-Allow-Methods', "GET, POST, OPTIONS, DELETE");
   next();
 });
 
